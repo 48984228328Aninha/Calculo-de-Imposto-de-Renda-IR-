b6bf63e2cfb33b5522a704d7727f5207c64cbcd1
48984228328Aninha <Amanda_rodrigues.de.siqueira@hotmail.com>
2026-05-28 19:08:56 -0300

    versão inicial programa imposto de renda

---
+++ Imposto.java
@@ -0,0 +1,21 @@
+
+public class Imposto {
+    private double salario_liquido;
+    private double salario_bruto;
+    private double inss;
+
+    public Imposto(double salario_liquido, double salario_bruto, double inss) {
+        this.salario_bruto = salario_bruto;
+        this.salario_liquido = salario_liquido;
+        this.inss = inss;
+    }
+
+public double calcularImposto(double inss, double salario_bruto) {
+    inss = salario_bruto * 
+}
+
+    public static void main(String[] args) {
+
+    }
+
+}

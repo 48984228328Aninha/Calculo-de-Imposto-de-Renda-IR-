dd0ff825f2a534e21f0691057c63db81709cecc6
48984228328Aninha <Amanda_rodrigues.de.siqueira@hotmail.com>
2026-06-03 20:05:05 -0300

    adicionar função verifica dependentes

--- Imposto.java
+++ Imposto.java
@@ -17,9 +17,17 @@ import javax.swing.JOptionPane;
 
 public class Imposto {
 
+    public double verificarDependentes(int dependente) {
+        double valorMensal = 189.59;
+        return dependente * valorMensal;
+    }
+
     public static void main(String[] args) {
+        Imposto calculadora = new Imposto();
+        double totalDeducao = 0;
+
         try {
-            Double salarioBruto = Double
+            Double salarioLiquido = Double
                     .parseDouble(JOptionPane.showInputDialog("Qual é o seu salário bruto?", ""));
 
         } catch (NumberFormatException e) {
@@ -27,7 +35,10 @@ public class Imposto {
         }
 
         try {
+            boolean temDependentes = JOptionPane.showConfirmDialog(null, "Você tem dependentes?", "Dependentes",
+                    JOptionPane.YES_NO_OPTION) == JOptionPane.YES_OPTION;
             int dependentes = Integer.parseInt(JOptionPane.showInputDialog("Quantos dependentes você tem?", ""));
+            totalDeducao = calculadora.verificarDependentes(dependentes);
 
         } catch (NumberFormatException e) {
             JOptionPane.showMessageDialog(null, "Digite um número válido.", "Número inválido", 0);
